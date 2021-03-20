cb7f7c254b09d742816264ab176a92887e54d0bc
caoyu <caoyu@uphicoo.com>
2021-03-20 10:21:43 +0800

    init

---
+++ HDSP.py
@@ -0,0 +1,31 @@
+print('c:\some\\name')
+
+print('py','thon')
+
+squares = [1, 4, 9, 24]
+squares
+
+
+"""
+1.列表数据结构squares
+列表的基本操作：append，inset，remove，pop、sort
+2.堆栈用法：append(追加) 、pop(弹出来),后进先出
+squares.append
+
+3.队列用法：先进先出 deque
+deque.append 、deque.popleft
+4.列表创建：list=[x*10 for x in range(10)]公式
+
+5.集合：sets 无排序
+6.元祖：Tuples 不可修改
+7.列表：Lists可修改
+8.词典：Dictionaries关系数据
+
+"""
+
+
+"""
+创建git 仓库远程链接  用户名caoyu110110 密码caoyu19934250
+
+
+"""
