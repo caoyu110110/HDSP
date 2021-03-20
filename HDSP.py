243713ee2d37fc5084972d3ad7637560cc8b4d68
caoyu <caoyu@uphicoo.com>
2021-03-20 10:35:17 +0800

    init

--- HDSP.py
+++ HDSP.py
@@ -27,5 +27,9 @@ deque.append 、deque.popleft
 """
 创建git 仓库远程链接  用户名caoyu110110 密码caoyu19934250
 
-
+git init 添加仓库
+git add README.md 添加说明文件（帮助文档）
+git commit -m "first commit" 提交文件
+git cgit remote add origin git@github.com:caoyu110110/HDSP.git
+git push -u origin main
 """
